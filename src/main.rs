6f2e0744de29f04601e04e8aef0b894d4d248bb4
Hugo van der Merwe <17109322+hugovdm@users.noreply.github.com>
2020-08-05 18:18:42 +0200

    Splice in wgpu_glyph's examples/hello.rs code.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-fn main() {
+use std::error::Error;
+use wgpu_glyph::{ab_glyph, GlyphBrushBuilder, Section, Text};
+
+fn main() -> Result<(), Box<dyn Error>> {
     use cpal::traits::{DeviceTrait, HostTrait};
     // use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
@@ -83,7 +86,9 @@ fn main() {
     };
     if let Err(e) = _stream {
         println!("Error from build_output_stream: {}", e);
-        return ();
+        // TODO: find a good way to terminate.
+        // return ();
+        panic!();
     }
     let _stream = _stream.unwrap();
 
@@ -92,4 +97,159 @@ fn main() {
             *sample = Sample::from(&0.0);
         }
     }
+
+    env_logger::init();
+
+    // Open window and create a surface
+    let event_loop = winit::event_loop::EventLoop::new();
+
+    let window = winit::window::WindowBuilder::new()
+        .with_resizable(false)
+        .build(&event_loop)
+        .unwrap();
+
+    let surface = wgpu::Surface::create(&window);
+
+    // Initialize GPU
+    let (device, queue) = futures::executor::block_on(async {
+        let adapter = wgpu::Adapter::request(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+            },
+            wgpu::BackendBit::all(),
+        )
+        .await
+        .expect("Request adapter");
+
+        adapter
+            .request_device(&wgpu::DeviceDescriptor {
+                extensions: wgpu::Extensions {
+                    anisotropic_filtering: false,
+                },
+                limits: wgpu::Limits { max_bind_groups: 1 },
+            })
+            .await
+    });
+
+    // Prepare swap chain
+    let render_format = wgpu::TextureFormat::Bgra8UnormSrgb;
+    let mut size = window.inner_size();
+
+    let mut swap_chain = device.create_swap_chain(
+        &surface,
+        &wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: render_format,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Mailbox,
+        },
+    );
+
+    // Prepare glyph_brush
+    let inconsolata = ab_glyph::FontArc::try_from_slice(include_bytes!(
+        "Inconsolata-Regular.ttf"
+    ))?;
+
+    let mut glyph_brush = GlyphBrushBuilder::using_font(inconsolata)
+        .build(&device, render_format);
+
+    // Render loop
+    window.request_redraw();
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            winit::event::Event::WindowEvent {
+                event: winit::event::WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = winit::event_loop::ControlFlow::Exit,
+            winit::event::Event::WindowEvent {
+                event: winit::event::WindowEvent::Resized(new_size),
+                ..
+            } => {
+                size = new_size;
+
+                swap_chain = device.create_swap_chain(
+                    &surface,
+                    &wgpu::SwapChainDescriptor {
+                        usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+                        format: render_format,
+                        width: size.width,
+                        height: size.height,
+                        present_mode: wgpu::PresentMode::Mailbox,
+                    },
+                );
+            }
+            winit::event::Event::RedrawRequested { .. } => {
+                // Get a command encoder for the current frame
+                let mut encoder = device.create_command_encoder(
+                    &wgpu::CommandEncoderDescriptor {
+                        label: Some("Redraw"),
+                    },
+                );
+
+                // Get the next frame
+                let frame =
+                    swap_chain.get_next_texture().expect("Get next frame");
+
+                // Clear frame
+                {
+                    let _ = encoder.begin_render_pass(
+                        &wgpu::RenderPassDescriptor {
+                            color_attachments: &[
+                                wgpu::RenderPassColorAttachmentDescriptor {
+                                    attachment: &frame.view,
+                                    resolve_target: None,
+                                    load_op: wgpu::LoadOp::Clear,
+                                    store_op: wgpu::StoreOp::Store,
+                                    clear_color: wgpu::Color {
+                                        r: 0.4,
+                                        g: 0.4,
+                                        b: 0.4,
+                                        a: 1.0,
+                                    },
+                                },
+                            ],
+                            depth_stencil_attachment: None,
+                        },
+                    );
+                }
+
+                glyph_brush.queue(Section {
+                    screen_position: (30.0, 30.0),
+                    bounds: (size.width as f32, size.height as f32),
+                    text: vec![Text::new("Hello wgpu_glyph!")
+                        .with_color([0.0, 0.0, 0.0, 1.0])
+                        .with_scale(40.0)],
+                    ..Section::default()
+                });
+
+                glyph_brush.queue(Section {
+                    screen_position: (30.0, 90.0),
+                    bounds: (size.width as f32, size.height as f32),
+                    text: vec![Text::new("Hello wgpu_glyph!")
+                        .with_color([1.0, 1.0, 1.0, 1.0])
+                        .with_scale(40.0)],
+                    ..Section::default()
+                });
+
+                // Draw the text!
+                glyph_brush
+                    .draw_queued(
+                        &device,
+                        &mut encoder,
+                        &frame.view,
+                        size.width,
+                        size.height,
+                    )
+                    .expect("Draw queued");
+
+                queue.submit(&[encoder.finish()]);
+            }
+            _ => {
+                *control_flow = winit::event_loop::ControlFlow::Wait;
+            }
+        }
+    });
 }
